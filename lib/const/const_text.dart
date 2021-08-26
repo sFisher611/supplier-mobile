32cf01383cf13e2140a533c87f6d13223073b7cb
Sirojiddin <ffiishsh@gmail.com>
2021-08-26 16:07:20 +0500

    const +

---
+++ lib/const/const_text.dart
@@ -0,0 +1,26 @@
+// ignore: non_constant_identifier_names
+final String TEXT_PRODUCT_DELIVERED = 'Йетказилиш керак товарлар';
+// ignore: non_constant_identifier_names
+final String TEXT_PRODUCT_LOADING = 'Юкланган';
+// ignore: non_constant_identifier_names
+final String TEXT_PRODUCT_CARD_BUTTON_DELIVERED = 'Етқзилди';
+// ignore: non_constant_identifier_names
+final String TEXT_PRODUCT_ACCEPTED = 'Қабул қилинган';
+// ignore: non_constant_identifier_names
+final String TEXT_CARD_BUTTON_ACCEPTED = 'Қабул қилиш';
+// ignore: non_constant_identifier_names
+final String TEXT_CARD_BUTTON_CANCEL = 'Бекор қилиш';
+// ignore: non_constant_identifier_names
+final String TEXT_FINISHED_TAB_CONDUCTED = 'Олиб борилган';
+// ignore: non_constant_identifier_names
+final String TEXT_FINISHED_TAB_BROUGHT = 'Олиб келинган';
+// ignore: non_constant_identifier_names
+final String TEXT_FINISHED_TITLE = 'Тугатилган ишлар';
+// ignore: non_constant_identifier_names
+final String TEXT_RETURN_TITLE = 'Олиб келиниш керак товарлар';
+// ignore: non_constant_identifier_names
+final String TEXT_RETURN_CARD_BUTTON = 'Қайтарилди';
+// ignore: non_constant_identifier_names
+String TEXT_BUTTON_DIALOG_CAMERA = 'Камера';
+// ignore: non_constant_identifier_names
+String TEXT_BUTTON_DIALOG_GALLERY = 'Галерея';
