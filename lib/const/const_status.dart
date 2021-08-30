d1d7cab4d3bc5ca42a56529f2c5167801c554d39
Sirojiddin <ffiishsh@gmail.com>
2021-08-30 17:41:00 +0500

    1+

---
+++ lib/const/const_status.dart
@@ -0,0 +1,10 @@
+// ignore: non_constant_identifier_names
+String STATUS_ORDER='БУЮРТМА';
+// ignore: non_constant_identifier_names
+String STATUS_ACCEPTED='ҚАБУЛ ҚИЛИНДИ';
+// ignore: non_constant_identifier_names
+String STATUS_FINISHED='ЕТКАЗИЛДИ';
+// ignore: non_constant_identifier_names
+String STATUS_RETURN_ORDER='ОЛИБ КЕЛИШ ҚАБУЛ ҚИЛИНДИ';
+// ignore: non_constant_identifier_names
+String STATUS_RETURN_FINISHED='ОЛИБ КЕЛИНДИ';
